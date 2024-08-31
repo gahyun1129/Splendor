a2fc0e8dfa3ecefaa390e20939fe31346629ccae
Gahyun <elfgh29@naver.com>
2024-08-31 11:53:57 +0900

    리소스 수정

---
+++ GameProject/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] blueSlot = null;
+    public GameObject[] greenSlot = null;
+    public GameObject[] yellowSlot = null;
+
+    public GameObject[] blueCards = null;
+    public GameObject[] greenCards = null;
+    public GameObject[] yellowCards = null;
+
+
+}
--- GameProject/Assets/Scripts/Game/Card.cs
+++ GameProject/Assets/Scripts/Game/Card.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CardType { white, green, black, red, blue };
+public enum CardType { white, blue, green, red, black, none };
 
 public class Card : MonoBehaviour
 {
     public int score;
     public CardType type;
     public int[] prices;
+
 
 }
